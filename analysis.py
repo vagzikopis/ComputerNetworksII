89f9a67c4b3cc613f11fef45d9107877841230fa
vagzikopis <vagzikopis@gmail.com>
2020-11-26 17:24:23 +0200

    Add R1 plot

--- analysis.py
+++ analysis.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 sns.set_style("darkgrid")
-path = "session-17-11-2020/"
+path = "session-25-11-2020/"
+echo_code = ""
+
 # # Echo Function
-# G1 Delay Response times
-df = pd.read_csv(path+"E2130.csv")
-print(df.ResponseTimes.mean())
-print(df.ResponseTimes.var())
+# # G1 Delay Response times
+# df = pd.read_csv(path+".csv")
+# print(df.ResponseTimes.mean())
+# print(df.ResponseTimes.var())
 # sns.lineplot(x=df.index, y=df.ResponseTimes, data=df, linewidth = 0.5)
-# plt.title('G1: Non-Delay Response Times')
+# plt.title('G1: Delay Response Times')
 # # Set y-axis label
 # plt.ylabel('Response Time (Milliseconds)')
 # # Set x-axis label
@@ -21,7 +23,7 @@ print(df.ResponseTimes.var())
 # plt.show()
 
 # # # G2 Delay Throughput
-# df = pd.read_csv(path+"RE2130.csv")
+# df = pd.read_csv(path+"R"+echo_code+".csv")
 # sns.lineplot(x=df.index, y=df.R, data=df, linewidth = 0.5)
 # plt.title('G2: Non-Delay Throughput')
 # # Set y-axis label
@@ -32,16 +34,16 @@ print(df.ResponseTimes.var())
 # plt.savefig(path+"G2.png")
 # plt.show()
 
-# # # G3 Non-Delay Response times
+# # G3 Non-Delay Response times
 # df = pd.read_csv(path+"E0000.csv")
 # print(df.ResponseTimes.mean())
 # print(df.ResponseTimes.var())
 # sns.lineplot(x=df.index, y=df.ResponseTimes, data=df, linewidth = 0.5)
 # plt.title('G3: Non-Delay Response Times')
 # # Set x-axis label
-# plt.xlabel('Milliseconds')
+# plt.xlabel('Packets')
 # # Set y-axis label
-# plt.ylabel('Packets')
+# plt.ylabel('Milliseconds')
 # plt.tight_layout()
 # plt.savefig(path+"G3.png")
 # plt.show()
@@ -59,7 +61,7 @@ print(df.ResponseTimes.var())
 # plt.show()
 
 # # G5 Delay Response Times Histogram
-# df = pd.read_csv(path+"E2130.csv")
+# df = pd.read_csv(path+"R"+echo_code+".csv")
 # sns.displot(df, x=df.ResponseTimes, bins=12)
 # plt.title('G5: Delay Response Times Histogram')
 # plt.tight_layout()
@@ -67,15 +69,16 @@ print(df.ResponseTimes.var())
 # plt.show()
 
 # # G6 Non-Delay Response Times Histogram
-# df = pd.read_csv(path+"E2130.csv")
-# sns.displot(df, x=df.ResponseTimes, bins=12)
+# df = pd.read_csv(path+"E0000.csv")
+# df = df[df.ResponseTimes < 500]
+# sns.displot(df, x=df.ResponseTimes)
 # plt.title('G6: Non-Delay Response Times Histogram')
 # plt.tight_layout()
 # plt.savefig(path+"G6.png")
 # plt.show()
 
 # # G7 Delay Throughput Histogram
-# df = pd.read_csv(path+"RE2130.csv")
+# df = pd.read_csv(path+"R"+echo_code+".csv")
 # sns.displot(df, x=df.R, bins=30)
 # plt.title('G7: Delay Throughput Times Histogram')
 # plt.tight_layout()
@@ -90,14 +93,38 @@ print(df.ResponseTimes.var())
 # plt.savefig(path+"G8.png")
 # plt.show()
 
-# TODO R1 
+# # TODO R1 
+# df = pd.read_csv(path+".csv")
+# print(df.head())
+# a = 0.7
+# b = 0.5
+# g = 2.7
+# df['SRTT'] = df['ResponseTimes']
+# df['sigma'] = df['ResponseTimes'] / 2
+# df['RTO'] = df['SRTT'] + 4 * df['sigma']
+# df['RTT'] = df['ResponseTimes']
+# for idx in df.index:
+#     if (idx!= 0):
+#         df.loc[idx, 'SRTT'] = a * df.loc[idx-1, 'SRTT'] + (1-a) * df.loc[idx, 'ResponseTimes']
+#         df.loc[idx, 'sigma'] = b * df.loc[idx-1, 'sigma'] + (1-b) * abs((df.loc[idx, 'SRTT']-df.loc[idx, 'ResponseTimes']))
+#         df.loc[idx, 'RTO'] = df.loc[idx, 'SRTT'] + g * df.loc[idx, 'sigma']
+# print(df.head())
+# sns.lineplot(data=df[['SRTT', 'RTO', 'RTT']])
+# plt.title("R1: SRTT & RTO")
+# # Set x-axis label
+# plt.ylabel('Milliseconds')
+# # Set y-axis label
+# plt.xlabel('Samples')
+# plt.tight_layout()
+# plt.savefig(path+"R1.png")    
+# plt.show()
 
 # # G9 DPCM song samples plot
 # df = pd.read_csv(path+"DPCM_SAMPLES_SONG.csv")
 # sns.lineplot(x=df.index, y=df.samples, linewidth = 0.1)
-# plt.title("G9: DPCM 'My Number One' Waveform")
+# plt.title("G9: DPCM 'At cell 33' Waveform")
 # # Set x-axis label
-# plt.ylabel('Frequency (Hz)')
+# plt.ylabel('')
 # # Set y-axis label
 # plt.xlabel('Samples')
 # plt.tight_layout()
@@ -109,7 +136,7 @@ print(df.ResponseTimes.var())
 # sns.lineplot(x=df.index, y=df.samples, linewidth = 0.1)
 # plt.title('G10: DPCM Frequency Generator Waveform')
 # # Set x-axis label
-# plt.ylabel('Frequency (Hz)')
+# plt.ylabel('')
 # # Set y-axis label
 # plt.xlabel('Samples')
 # plt.tight_layout()
@@ -119,7 +146,7 @@ print(df.ResponseTimes.var())
 # # G11 DPCM song samples distribution
 # df = pd.read_csv(path+"DPCM_SAMPLES_SONG.csv")
 # sns.displot(df, x=df.samples, bins=15, kde=True)
-# plt.title("G11: DPCM 'My Number One' Samples Distribution")
+# plt.title("G11: DPCM 'At cell 33' Samples Distribution")
 # plt.tight_layout()
 # plt.savefig(path+"G11.png")
 # plt.show()
@@ -127,31 +154,31 @@ print(df.ResponseTimes.var())
 # # G12 DPCM song differences distribution
 # df = pd.read_csv(path+"DPCM_DIFFS_GENERATOR.csv")
 # sns.displot(df, x=df.differences, bins=15, kde=True)
-# plt.title("G12: DPCM 'My Number One' Differences Distribution")
+# plt.title("G12: DPCM 'At cell 33' Differences Distribution")
 # plt.tight_layout()
 # plt.savefig(path+"G12.png")
 # plt.show()
 
 # # G13 AQDPCM song1 samples distribution
-# df = pd.read_csv(path+"AQDPCM_SAMPLES_SONG_L01.csv")
+# df = pd.read_csv(path+"AQDPCM_SAMPLES_SONG_L33.csv")
 # sns.displot(df, x=df.samples, bins=15, kde=True)
-# plt.title("G13: AQDPCM 'My Number One' Samples Distribution")
+# plt.title("G13: AQDPCM 'At cell 33' Samples Distribution")
 # plt.tight_layout()
 # plt.savefig(path+"G13.png")
 # plt.show()
 
 # # G14 AQDPCM song1 differences distribution
-# df = pd.read_csv(path+"AQDPCM_DIFFS_SONG_L01.csv")
+# df = pd.read_csv(path+"AQDPCM_DIFFS_SONG_L33.csv")
 # sns.displot(df, x=df.differences, bins=15, kde=True)
-# plt.title("G14: AQDPCM 'My Number One' Differences Distribution")
+# plt.title("G14: AQDPCM 'At cell 33' Differences Distribution")
 # plt.tight_layout()
 # plt.savefig(path+"G14.png")
 # plt.show()
 
 # # G15 AQDPCM Song1 means plot
-# df = pd.read_csv(path+"AQDPCM_MEANS_SONG_L01.csv")
+# df = pd.read_csv(path+"AQDPCM_MEANS_SONG_L33.csv")
 # sns.lineplot(x=df.index, y=df.means, linewidth = 0.5)
-# plt.title("G15: AQDPCM 'My Number One' Mean Values")
+# plt.title("G15: AQDPCM 'At cell 33' Mean Values")
 # # Set x-axis label
 # plt.ylabel('Mean')
 # # Set y-axis label
@@ -161,9 +188,9 @@ print(df.ResponseTimes.var())
 # plt.show()
 
 # # G16 AQDPCM Song1 steps plot
-# df = pd.read_csv(path+"AQDPCM_STEPS_SONG_L01.csv")
+# df = pd.read_csv(path+"AQDPCM_STEPS_SONG_L33.csv")
 # sns.lineplot(x=df.index, y=df.steps, linewidth = 0.5)
-# plt.title("G16: AQDPCM 'My Number One' Step Values")
+# plt.title("G16: AQDPCM 'At cell 33' Step Values")
 # # # Set x-axis label
 # plt.ylabel('Steps')
 # # Set y-axis label
@@ -173,9 +200,9 @@ print(df.ResponseTimes.var())
 # plt.show()
 
 # # G17 AQDPCM Song2 means plot
-# df = pd.read_csv(path+"AQDPCM_MEANS_SONG_L03.csv")
+# df = pd.read_csv(path+"AQDPCM_MEANS_SONG_L20.csv")
 # sns.lineplot(x=df.index, y=df.means, linewidth = 0.5)
-# plt.title("G17: AQDPCM 'Symphony No.9' Mean Values")
+# plt.title("G17: AQDPCM 'That's my name' Mean Values")
 # # Set x-axis label
 # plt.ylabel('Mean')
 # # Set y-axis label
@@ -185,9 +212,9 @@ print(df.ResponseTimes.var())
 # plt.show()
 
 # # G18 AQDPCM Song2 steps plot
-# df = pd.read_csv(path+"AQDPCM_STEPS_SONG_L03.csv")
+# df = pd.read_csv(path+"AQDPCM_STEPS_SONG_L20.csv")
 # sns.lineplot(x=df.index, y=df.steps, linewidth = 0.5)
-# plt.title("G18: AQDPCM 'Symphony No.9' Step Values")
+# plt.title("G18: AQDPCM 'That's my name' Step Values")
 # # Set x-axis label
 # plt.ylabel('Steps')
 # # Set y-axis label
@@ -239,7 +266,7 @@ print(df.ResponseTimes.var())
 #     df = df.join(temp)
 
 # colors = plt.rcParams["axes.prop_cycle"]()
-# f, axes = plt.subplots(3, 2)
+# f, axes = plt.subplots(3, 2, figsize=(10,8))
 # f.suptitle("OnBoard Diagnostics")
 # c = next(colors)["color"]
 # axes[0][0].title.set_text('Engine Runtime')
